57047e4cdcae9f96a1c5236e3251020c89ce4f3f
Patrick Canfield <pscale01@gmail.com>
2023-10-11 17:09:24 -0700

    implement some of breakout

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,78 @@
-let count = 0;
-
-function handleClick() {
-  count++;
-  console.log(count);
-}
+import * as Phaser from 'phaser';
 
 if (module.hot) {
   module.hot.accept(() => {
-    document.body.removeEventListener('click', handleClick);
   });
 }
 
-document.body.addEventListener('click', handleClick);
+let ball: Phaser.Physics.Arcade.Sprite;
+let paddle: Phaser.Physics.Arcade.Sprite;
+let violetBricks: Phaser.Physics.Arcade.Group;
+let yellowBricks: Phaser.Physics.Arcade.Group;
 
-console.log('Hello World!!');
+class MyScene extends Phaser.Scene {
+  preload () {
+  this.load.image('ball', 'assets/images/ball_32_32.png');
+  this.load.image('paddle', 'assets/images/paddle_128_32.png');
+  this.load.image('brick1', 'assets/images/brick1_64_32.png');
+  this.load.image('brick2', 'assets/images/brick2_64_32.png');
+  this.load.image('brick3', 'assets/images/brick3_64_32.png');
+  }
+  create () {
+    // create a sprite
+    ball = this.physics.add.sprite(
+      400, // x position
+      565, // y position
+      'ball' // key of image for the sprite
+    );
+    paddle = this.physics.add.sprite(
+      400, // x position
+      600, // y position
+      'paddle' // key of image for the sprite
+    );
+    // create a group
+    violetBricks = this.physics.add.group({
+      key: 'brick1',
+      repeat: 9,
+      immovable: true,
+      setXY: {
+        x: 80,
+        y: 140,
+        stepX: 70
+      }
+    });
+    yellowBricks = this.physics.add.group({
+      key: 'brick2',
+      repeat: 9,
+      immovable: true,
+      setXY: {
+        x: 80,
+        y: 90,
+        stepX: 70
+      }
+    });
+  }
+  update () {
+  }
+}
+
+const game = new Phaser.Game({
+  type: Phaser.AUTO, // try WebGL, fallback to Canvas
+  parent: 'game', // element ID
+  width: 800,
+  height: 640,
+  scale: {
+    mode: Phaser.Scale.RESIZE,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  scene: MyScene,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: {x: 0, y: 0},
+    },
+  }
+});
+
+
+
