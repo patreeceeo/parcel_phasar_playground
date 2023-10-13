2d88300c4a4e8e47300329617d9e443d2dd50525
Patrick Canfield <pscale01@gmail.com>
2023-10-12 20:11:12 -0700

    do some more breakout

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import * as Phaser from 'phaser';
+import type { Types as PhaserTypes } from 'phaser';
 
 if (module.hot) {
   module.hot.accept(() => {
@@ -9,6 +10,9 @@ let ball: Phaser.Physics.Arcade.Sprite;
 let paddle: Phaser.Physics.Arcade.Sprite;
 let violetBricks: Phaser.Physics.Arcade.Group;
 let yellowBricks: Phaser.Physics.Arcade.Group;
+let cursors: Phaser.Types.Input.Keyboard.CursorKeys | undefined;
+let gameStarted = false;
+let randNum = 0;
 
 class MyScene extends Phaser.Scene {
   preload () {
@@ -25,11 +29,16 @@ class MyScene extends Phaser.Scene {
       565, // y position
       'ball' // key of image for the sprite
     );
+    ball.setCollideWorldBounds(true);
+    ball.setBounce(1)
+
     paddle = this.physics.add.sprite(
       400, // x position
       600, // y position
       'paddle' // key of image for the sprite
     );
+    paddle.setCollideWorldBounds(true);
+    paddle.setImmovable(true);
     // create a group
     violetBricks = this.physics.add.group({
       key: 'brick1',
@@ -51,8 +60,36 @@ class MyScene extends Phaser.Scene {
         stepX: 70
       }
     });
+    cursors = this.input.keyboard?.createCursorKeys();
+
+    this.physics.add.collider(ball, violetBricks, hitBrick, undefined, this);
+    this.physics.add.collider(ball, yellowBricks, hitBrick, undefined, this);
+    this.physics.add.collider(ball, paddle, hitPlayer, null, this);
   }
   update () {
+    // Check if the ball left the scene i.e. game over
+    if (isGameOver(this.physics.world)) {
+      // TODO: Show "Game over" message to the player
+    } else if (isWon()) {
+      // TODO: Show "You won!" message to the player
+    } else {
+      // Put this in so that the player stays still if no key is being pressed
+      setVelocityX(paddle, 0)
+
+      if (cursors?.left.isDown) {
+        setVelocityX(paddle, -350)
+      } else if (cursors?.right.isDown) {
+        setVelocityX(paddle, 350)
+      }
+      if (!gameStarted) {
+        ball.setX(paddle.x);
+
+        if (cursors?.space.isDown) {
+          gameStarted = true;
+          ball.setVelocityY(-200);
+        }
+      }
+    }
   }
 }
 
@@ -75,4 +112,62 @@ const game = new Phaser.Game({
 });
 
 
+function isGameOver(world: Phaser.Physics.Arcade.World) {
+  return ball.body && ball.body.y > world.bounds.height;
+}
 
+function isWon() {
+  return violetBricks.countActive() + yellowBricks.countActive() == 0;
+}
+
+function setVelocityX(sprite: Phaser.Physics.Arcade.Sprite, velocity: number) {
+  if (sprite.body) {
+    sprite.body.velocity.x = velocity;
+  }
+}
+function getVelocityX(sprite: Phaser.Physics.Arcade.Sprite) {
+  if (sprite.body) {
+    return sprite.body.velocity.x
+  } else {
+    return 0
+  }
+}
+function setVelocityY(sprite: Phaser.Physics.Arcade.Sprite, velocity: number) {
+  if (sprite.body) {
+    sprite.body.velocity.y = velocity;
+  }
+}
+function getVelocityY(sprite: Phaser.Physics.Arcade.Sprite) {
+  if (sprite.body) {
+    return sprite.body.velocity.x
+  } else {
+    return 0
+  }
+}
+
+const hitBrick: PhaserTypes.Physics.Arcade.ArcadePhysicsCallback = ((ball: Phaser.Physics.Arcade.Sprite, brick: Phaser.Physics.Arcade.Sprite) => {
+  brick.disableBody(true, true);
+
+  if (getVelocityX(ball) == 0) {
+    randNum = Math.random();
+    if (randNum >= 0.5) {
+      setVelocityX(ball, 150);
+    } else {
+      setVelocityX(ball, -150);
+    }
+  }
+}) as any;
+
+function hitPlayer(ball: Phaser.Physics.Arcade.Sprite, player: Phaser.Physics.Arcade.Sprite) {
+  // Increase the velocity of the ball after it bounces
+  const ballVelocityY = getVelocityY(ball);
+  ball.setVelocityY(ballVelocityY - 5)
+
+  let newXVelocity = Math.abs(getVelocityX(ball) + 5);
+  // If the ball is to the left of the player, ensure the X Velocity is negative
+  if (ball.x < player.x) {
+    ball.setVelocityX(-newXVelocity);
+  } else {
+    ball.setVelocityX(newXVelocity);
+  }
+}
